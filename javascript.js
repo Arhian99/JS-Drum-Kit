8361fca5bbe9c905f9374bff560f4516b3b11b70
Arhian Albis Ramos <108996128+Arhian99@users.noreply.github.com>
2022-09-02 17:00:37 -0500

    add interactive key feature on keydown event

--- javascript.js
+++ javascript.js
@@ -1,19 +1,113 @@
 
 const keys = document.getElementsByClassName('key');
 
-
-/*const a = document.getElementById('a');
-a.addEventListener('mouseover', () => a.className = 'key-interact');
-a.addEventListener('mouseleave', () => a.className = 'key');
-
-
-for (element in keys) {
-    let key = element
-    key.addEventListener('mouseover', () => a.className = 'key-interact')
-} */
-
 for (i=0; i < keys.length; i++) {
     let key = keys[i];
     key.addEventListener('mouseover', () => key.className = 'key-interact');
     key.addEventListener('mouseleave', () => key.className = 'key');
-}
+};
+
+
+
+//window.addEventListener('keyup', reset);
+
+
+function checkKeyPressDown(event) {
+    if (event.keyCode == 65) {
+        let id = document.getElementById('a');
+        id.className = 'key-interact';
+    }
+
+    else if (event.keyCode == 83) {
+        let id = document.getElementById('s');
+        id.className = 'key-interact';
+    }
+
+    else if (event.keyCode == 68) {
+        let id = document.getElementById('d');
+        id.className = 'key-interact';
+    }
+
+    else if (event.keyCode == 70) {
+        let id = document.getElementById('f');
+        id.className = 'key-interact';
+    }
+
+    else if (event.keyCode == 71) {
+        let id = document.getElementById('g');
+        id.className = 'key-interact';
+    }
+
+    else if (event.keyCode == 72) {
+        let id = document.getElementById('h');
+        id.className = 'key-interact';
+    }
+
+    else if (event.keyCode == 74) {
+        let id = document.getElementById('j');
+        id.className = 'key-interact';
+    }
+
+    else if (event.keyCode == 75) {
+        let id = document.getElementById('k');
+        id.className = 'key-interact';
+    }
+
+    else if (event.keyCode == 76) {
+        let id = document.getElementById('l');
+        id.className = 'key-interact';
+    }
+};
+
+function checkKeyPressUp(event) {
+    if (event.keyCode == 65) {
+        let id = document.getElementById('a');
+        id.className = 'key';
+    }
+
+    else if (event.keyCode == 83) {
+        let id = document.getElementById('s');
+        id.className = 'key';
+    }
+
+    else if (event.keyCode == 68) {
+        let id = document.getElementById('d');
+        id.className = 'key';
+    }
+
+    else if (event.keyCode == 70) {
+        let id = document.getElementById('f');
+        id.className = 'key';
+    }
+
+    else if (event.keyCode == 71) {
+        let id = document.getElementById('g');
+        id.className = 'key';
+    }
+
+    else if (event.keyCode == 72) {
+        let id = document.getElementById('h');
+        id.className = 'key';
+    }
+
+    else if (event.keyCode == 74) {
+        let id = document.getElementById('j');
+        id.className = 'key';
+    }
+
+    else if (event.keyCode == 75) {
+        let id = document.getElementById('k');
+        id.className = 'key';
+    }
+
+    else if (event.keyCode == 76) {
+        let id = document.getElementById('l');
+        id.className = 'key';
+    }
+};
+
+
+
+window.addEventListener('keydown', checkKeyPressDown);
+window.addEventListener('keyup', checkKeyPressUp);
+
